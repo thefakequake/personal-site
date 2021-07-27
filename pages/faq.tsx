c8023fbc7cb2bd910447190e1ec28db8ba442625
QuaKe8782 <mastermind4560@gmail.com>
2021-07-27 12:32:05 +0100

    Add title to FAQ page

--- pages/faq.tsx
+++ pages/faq.tsx
@@ -1,10 +1,14 @@
 import Divider from "@/components/Divider"
 import { faq } from "@/data"
 import styles from "@/styles/faq.module.css"
+import Head from "next/head"
 
 const FaqPage = () => {
   return (
     <>
+      <Head>
+        <title>FAQ</title>
+      </Head>
       <h1 className="gradient">FAQ</h1>
       <Divider thickness={2} width="narrow" gradient={true} align="left" />
       {faq.map(([question, answer], count) => (
